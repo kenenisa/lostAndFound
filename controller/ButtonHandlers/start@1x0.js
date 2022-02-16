c06f94c8ce2d86cce3fd68491090b1c794d31c66
kenenisa alemayehu <kenenisaalemayhu0@gmail.com>
2022-02-16 11:30:09 +0300

    moving to local

--- controller/ButtonHandlers/start@1x0.js
+++ controller/ButtonHandlers/start@1x0.js
@@ -1,9 +1,9 @@
 
-const { User } = require('./../../models/db.js');
+const { User, Item } = require('./../../models/db.js');
 const dayjs = require('dayjs')
 const calendar = require('dayjs/plugin/calendar');
 
 dayjs.extend(calendar)
-module.exports = (ctx, message) => {
-    ctx.reply('tf u wanna know?')
+module.exports = async (ctx, message) => {
+    await ctx.replyWithHTML('<b>About</b>')
 }
